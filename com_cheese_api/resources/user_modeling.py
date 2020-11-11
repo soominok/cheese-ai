b0a26a6d998b1b4ce42504eacb77c234404d9fe8
soominok <sumin00331@naver.com>
2020-11-11 09:54:12 +0900

    softmax commit

--- com_cheese_api/resources/user_modeling.py
+++ com_cheese_api/resources/user_modeling.py
@@ -1,27 +1,27 @@
-import numpy as np
-import pandas as pd
-from com_cheese_api.util.file import FileReader
-from pathlib import Path
-# from com_cheese_api.ext.db import url, db, openSession, engine
-# from konlpy.tag import Okt
-# from collections import Counter
-# from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
-import seaborn as sns
-from sqlalchemy import func
-from sqlalchemy.ext.declarative import declarative_base
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold  # k value is understood as count
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestClassifier # rforest
-from sklearn.tree import DecisionTreeClassifier # dtree
-from sklearn.ensemble import RandomForestClassifier # rforest
-from sklearn.naive_bayes import GaussianNB # nb
-from sklearn.neighbors import KNeighborsClassifier # knn
-from sklearn.svm import SVC # svm
+# import numpy as np
+# import pandas as pd
+# from com_cheese_api.util.file import FileReader
+# from pathlib import Path
+# # from com_cheese_api.ext.db import url, db, openSession, engine
+# # from konlpy.tag import Okt
+# # from collections import Counter
+# # from wordcloud import WordCloud
+# import matplotlib.pyplot as plt
+# import matplotlib.font_manager as fm
+# import seaborn as sns
+# from sqlalchemy import func
+# from sqlalchemy.ext.declarative import declarative_base
+# from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import KFold  # k value is understood as count
+# from sklearn.model_selection import cross_val_score
+# from sklearn.ensemble import RandomForestClassifier # rforest
+# from sklearn.tree import DecisionTreeClassifier # dtree
+# from sklearn.ensemble import RandomForestClassifier # rforest
+# from sklearn.naive_bayes import GaussianNB # nb
+# from sklearn.neighbors import KNeighborsClassifier # knn
+# from sklearn.svm import SVC # svm
 
-import os
+# import os
 
 # class UserModel:
 #     def __init__(self):
@@ -55,16 +55,29 @@ import os
 #     userModel = UserModel()
 #     userModel.new()
 
-class UserModel:
-    def __init__(self):
-        self.odf = None
+# class UserModel:
+#     def __init__(self):
+#         self.odf = None
 
-    @staticmethod
-    def readData():
-        users = pd.read_csv("com_cheese_api/resources/data/users.csv")
+#     @staticmethod
+#     def readData():
+#         users = pd.read_csv("com_cheese_api/resources/data/users.csv")
 
-        print(users)
+#         print(users)
 
-if __name__ == '__main__':
-    userModel = UserModel()
-    userModel.readData()
+# if __name__ == '__main__':
+#     userModel = UserModel()
+#     userModel.readData()
+
+
+import pandas as pd
+
+users = pd.read_csv("resources/data/user_dataset.csv")
+
+# user_X_train_set = user_X_train.drop('user_id', axis = 1)
+users['cheese_id'] = users['cheese_id'].str.replace('p','')
+users['cheese_one_price'] = users['cheese_one_price'].str.replace(',', '')
+users['cheese_one_price'] = users['cheese_one_price'].str.replace('원', '')
+users = users.astype({'cheese_one_price': int})
+
+# user_X_trian_set = user_X_train_set.astype({'cheese_one_price': int})
